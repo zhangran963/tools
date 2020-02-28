623989a412792cb69e177440115e9f1aac9eb2e0
ran <zhangran963@gmail.com>
2020-02-28 16:08:29 +0800

    fix: 修复try_file命令
    1. 判断是否是文件(增);
    2. 再判断此文件是否存在;

--- static-server/try_files.js
+++ static-server/try_files.js
@@ -1,7 +1,9 @@
 // try_files指令: 按顺序检查文件是否存在，返回第一个找到的文件或文件夹(结尾加斜线表示为文件夹)，如果所有的文件或文件夹都找不到，会进行一个内部重定向到最后一个参数
 const fs = require('fs')
+const path = require('path')
 
-
-module.exports = function try_files(...paths){
-  return paths.find(path => fs.existsSync(path))
+module.exports = function try_files(...urls){
+  return urls.find(url => /* 文件格式 && 存在 */
+    path.extname(url).length>1 && fs.existsSync(url)
+  )
 }
